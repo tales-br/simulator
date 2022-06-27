8be27e5969fad89921f792e1fbb7ce86c9507073
Tales <20765261+tales-br@users.noreply.github.com>
2022-06-27 17:45:35 -0300

    Developed Main Layout and Detail Layout

---
+++ app/src/main/java/br/tales/simulator/DetailActivity.kt
@@ -0,0 +1,20 @@
+package br.tales.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.tales.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
