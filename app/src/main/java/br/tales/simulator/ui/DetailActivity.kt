0e8215038cbf9ce2cda91e7c7585b0864a717194
Tales <20765261+tales-br@users.noreply.github.com>
2022-07-06 16:02:54 -0300

    Detail Activity implemented

--- app/src/main/java/br/tales/simulator/ui/DetailActivity.kt
+++ app/src/main/java/br/tales/simulator/ui/DetailActivity.kt
@@ -33,6 +33,25 @@ class DetailActivity : AppCompatActivity()
         intent?.extras?.getParcelable<Match>(Extras.MATCH)?.let {
             Glide.with(this).load(it.local.image).into(binding.ivPlace)
             supportActionBar?.title = it.local.name
+
+            binding.tvDescription.text = it.description
+
+            Glide.with(this).load(it.homeTeam.image).into(binding.ivHomeTeam)
+            binding.tvHomeTeamName.text = it.homeTeam.name
+            binding.rbHomeTeamStars.rating = it.homeTeam.stars.toFloat()
+            if(it.homeTeam.score != null)
+            {
+                binding.tvHomeTeamScore.text = it.homeTeam.score.toString()
+            }
+
+            Glide.with(this).load(it.awayTeam.image).into(binding.ivAwayTeam)
+            binding.tvAwayTeamName.text = it.awayTeam.name
+            binding.rbAwayTeamStars.rating = it.awayTeam.stars.toFloat()
+            if(it.awayTeam.score != null)
+            {
+                binding.tvAwayTeamScore.text = it.awayTeam.score.toString()
+            }
+
         }
     }
 }
